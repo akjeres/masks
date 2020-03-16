2293b8dab99beaaa2ec5803e5bde803bcf71c5cc
akjeres <akjeres@gmail.com>
2020-03-16 23:19:07 +0200

    Updates

---
+++ precache-manifest.3d0d8b7a60edba2fa7a4dab843c40388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b68851329cec6f934445cb13cf363d",
+    "url": "/masks/index.html"
+  },
+  {
+    "revision": "3870825f92b68865abe4",
+    "url": "/masks/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "cbb3fc0b53f96aae7807",
+    "url": "/masks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3870825f92b68865abe4",
+    "url": "/masks/static/js/2.2a337b2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/masks/static/js/2.2a337b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb3fc0b53f96aae7807",
+    "url": "/masks/static/js/main.34137c83.chunk.js"
+  },
+  {
+    "revision": "88cb8a1f43e48cf8e0dc",
+    "url": "/masks/static/js/runtime-main.93ecd221.js"
+  }
+]);
