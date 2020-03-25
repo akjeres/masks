46a0e6171c4997e604fce1bf5e0fa4c60a456ee2
akjeres <akjeres@gmail.com>
2020-03-26 00:27:47 +0200

    Spinner's been added

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './spinner.css';
+
+export const Spinner = () => {
+  return (
+      <div className="preloader">
+          <div className="loadingio-spinner-chunk-iu8m6z8safl">
+              <div className="ldio-h8jpm9maxb8">
+                  <div>
+                      <div>
+                          <div></div>
+                          <div></div>
+                          <div></div>
+                          <div></div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+};
